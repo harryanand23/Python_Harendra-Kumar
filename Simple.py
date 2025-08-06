ee09b8de7621043215a50e81730acc737e844433
harryanand23 <harryanand23@gmail.com>
2025-08-06 20:40:14 +0530

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,17 @@
+# Task 1: Perform Basic Mathematical Operations
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+c=int(c)
+d=int(d)
+e=int(e)
+f=float(f)
+print("Addition: ",c)
+print("Subtraction: ",d)
+print("Multiplication: ",e)
+print("Division: ",f)
